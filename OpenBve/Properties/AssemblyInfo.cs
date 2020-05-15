ac51e7bda26ff7d5642172fe65f38531c5328c99
AlteredCarrot71 <gabesz790409@gmail.com>
2020-05-15 10:48:13 +0200

    Initial import

---
+++ OpenBve/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("openBVE")]
+[assembly: AssemblyProduct("openBVE")]
+[assembly: AssemblyCopyright("(Public Domain) http://trainsimframework.org/")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bd68500e-8db6-4394-8fec-6adcde64c213")]
+[assembly: AssemblyVersion("1.2.13.0")]
+[assembly: AssemblyFileVersion("1.2.13.0")]
+[assembly: CLSCompliant(true)]
+
+namespace OpenBve {
+	public static partial class Program {
+		internal const bool IsDevelopmentVersion = false;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # openBVE_remake
 Remake of openBVE 1.2 version
+
+[![CodeFactor](https://www.codefactor.io/repository/github/alteredcarrot71/openbve_remake/badge)](https://www.codefactor.io/repository/github/alteredcarrot71/openbve_remake)
